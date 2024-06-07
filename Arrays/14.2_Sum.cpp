e8f3d4fad6042cf712d98ffe047ceb65060ec551
Manan Parakh <mananparakh500@gmail.com>
2024-06-07 16:52:16 +0530

    Map, 2-pointer greedy approach

--- Arrays/14.2_Sum.cpp
+++ Arrays/14.2_Sum.cpp
@@ -37,7 +37,7 @@ void betterAB(int a[], int size, int target)
     // Now start parsing the array, for every element look if target - element exists or not -> O(n)
 
     //Hash the array -> O(n)
-    
+
     map<int,int> mpp;
     for (int i = 0; i < size; i++)
     {
@@ -51,14 +51,53 @@ void betterAB(int a[], int size, int target)
         if (mpp.find(target - a[i]) != mpp.end())
         {
             flag = 1;
-            cout << i << endl;
+            cout << "Index_1:" << i << endl;  
+            cout << "Index_2:" << mpp[target - a[i]];
+            break;
         }
     }
     if(!flag)
         cout << "No";
-    //TC -> 
+    
+    //TC -> O(n*log(n)) : Ordered Map
+    //TC -> O(n*1) : Unordered Map(best and avg case), O(n*n) : Worst Case
+    //Find takes O(N) in the worst case of Unordered Map
+    //SC -> O(n)
 }
 
+//To solve it without using the map datastructure -> 2 Pointer Greedy Approach
+//It is just slightly better than betterAB
+void optimalAB(int a[], int size, int target)
+{
+    //Sort the array
+    //Take sum of 1st and last element
+    //If sum > target, j--
+    //If sum < target, i++
+    sort(a, a + size);
+    int i = 0; 
+    int j = size-1;
+    while (i<j)
+    {
+        if (a[i] + a[j] == target)
+        {
+            cout << "Yes!" << endl;
+            cout << "Element_1:" << a[i] << endl;
+            cout << "Element_2:" << a[j] << endl;
+            return;
+        }
+        else if (a[i] + a[j] < target)
+        {
+            i++;
+        }
+        else j--;
+    }
+    cout << "No";
+    //TC -> O(n*logn) + O(n)
+    //SC -> O(1)
+
+
+    // For part B, store the array in some other data structure along with the element's indices : (elements, indices) and then sort it.
+}
 int main()
 {
     //A. Given an array and a target value, find if the array contains 2 elements such that their sum is equal to the target value given.
@@ -67,5 +106,6 @@ int main()
     int target = 14;
     int size = sizeof(arr)/4;
     //bruteAB(arr,size,target);
-    betterAB(arr,size,target);
+    //betterAB(arr,size,target);
+    optimalAB(arr,size,target);
 }
