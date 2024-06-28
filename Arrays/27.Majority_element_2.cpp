36861a8401f742f859261279c69bf589149066a2
Manan Parakh <mananparakh500@gmail.com>
2024-06-28 16:47:02 +0530

    Will Continue later. Let's Focus on topics ahead!

---
+++ Arrays/27.Majority_element_2.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//An idea that did not work. Nonetheless let's preserve the logic
+vector<int> first_try(vector<int>& vec)
+{
+    vector<int> ans;
+    // Create the variables to store 2 distinct digits along wit their counters
+    int val1 = -1, val2 = -1;
+    float cnt1 = 0, cnt2 = 0;
+
+    for (auto ele : vec)
+    {
+        if (val1 == -1 || cnt1 == 0 || val1 == ele)
+        {
+            val1 = ele;
+            cnt1++;
+            cnt2 = cnt2 - 0.5;
+        }
+        else if (val2 == -1 || cnt2 == 0 || val2 == ele)
+        {
+            val2 = ele;
+            cnt2++;
+            cnt1 = cnt1 - 0.5;
+        }
+        else
+        {
+            cnt1 = cnt1 - 0.5;
+            cnt2 = cnt2 - 0.5;
+        }
+    }
+
+    if (cnt1 > 0)
+    {
+        ans.push_back(val1);
+    }
+    if (cnt2 > 0)
+    {
+        ans.push_back(val2);
+    }
+    return ans; 
+}
+
+vector<int> 
+int main()
+{
+    vector<int> vec = {1, 1, 3,1, 3, 2, 2, 2};
+    for (auto ele : first_try(vec))
+    {
+        cout << ele << " ";
+    }
+}
