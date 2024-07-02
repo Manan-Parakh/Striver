0c4a0216ed931bd0ea938bc26edc22e0de928bb9
Manan Parakh <mananparakh500@gmail.com>
2024-07-02 22:29:44 +0530

    Compiler Error, but ok

---
+++ Binary Search/11.Nth_Root.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Compiler Error, but ok
+int NthRoot(int n, int m)
+{
+    int low = 0, high = m;
+    while (low <= high)
+    {
+        int mid = (high + low) / 2;
+        if (pow(mid, n) == m)
+        {
+            return mid;
+        }
+        else if (pow(mid, n) > m)
+            high = mid - 1;
+        else if (pow(mid, n) < m)
+        {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    cout << NthRoot(4, 81);
+}
